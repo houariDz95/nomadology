3a022c5023f716734ee7323e6a901d0a56fea343
eddDevDz95 <houedd608@gmail.com>
2024-12-01 12:52:19 +0100

    new commit

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -6,7 +6,7 @@ const siteMetadata = {
     description: 'Nomadology is your go-to travel and lifestyle blog for digital nomads. Explore top remote work destinations, budget travel tips, and essential gear reviews. Perfect for modern travelers and aspiring nomads seeking adventure and freedom.',  
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://nomadology.vercel.app/', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
     email: 'nomadology22@gmail.com', 
