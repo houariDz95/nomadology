90cb09c620955abdc0327c0003be9a3948ee51b4
eddDevDz95 <houedd608@gmail.com>
2024-12-01 13:27:07 +0100

    new commit

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -1,3 +1,4 @@
+import BlogDetails from "@/src/components/Blog/BlogDetails";
 import RenderMdx from "@/src/components/Blog/RenderMdx";
 import Tag from "@/src/components/Elements/Tag";
 import siteMetadata from "@/src/utils/siteMetaData";
@@ -119,7 +120,6 @@ export default function BlogPage({ params }) {
           sizes="100vw"
         />
       </div>
-
 
       <div className="grid grid-cols-12  gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
         <div className="col-span-12  lg:col-span-4">
