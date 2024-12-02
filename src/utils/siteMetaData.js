a32d2cb8fd7ce0b26d41aec1c797c8e5a32f2dec
eddDevDz95 <houedd608@gmail.com>
2024-12-02 21:58:58 +0100

    new update

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,9 +1,9 @@
 // please update required information
 const siteMetadata = {
-    title: 'Nomadology - Digital Nomad Destinations, Tips & Resources for Remote Workers',
+    title: 'Nomadology - Explore Top Digital Nomad Destinations & Remote Work Tips',
     author: 'Houari Eddrief',
     headerTitle: 'Nomadology',
-    description: 'Nomadology is your go-to travel and lifestyle blog for digital nomads. Explore top remote work destinations, budget travel tips, and essential gear reviews. Perfect for modern travelers and aspiring nomads seeking adventure and freedom.',  
+    description: 'Discover the best digital nomad destinations, practical remote work tips, budget travel hacks, and gear reviews. Nomadology is the ultimate guide for modern nomads seeking adventure and freedom.',  
     language: 'en-us',
     theme: 'system', // system, dark or light
     siteUrl: 'https://nomadology.vercel.app', // your website URL
