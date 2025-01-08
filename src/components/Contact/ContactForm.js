01de7b7a8f69d4287d1791b17558d44209420985
eddDevDz95 <houedd608@gmail.com>
2025-01-09 00:50:37 +0100

    new commit

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,10 +1,48 @@
-"use client"; // Ensure it's a client-side component
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 
 export default function ContactForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [loading, setLoading] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  
+  const onSubmit = (data) => {
+    setLoading(true);
+    console.log(data);
+    emailjs
+    .send(
+      "service_3dmchep",
+      "template_vaxg1bh",
+      {
+        from_name: data.name,
+        to_name: "Nomadology",
+        from_email: data.email,
+        phone: data.phone,
+        to_email: "nomadology22@gmail.com",
+        message: data.details,
+      },
+      "2DgNSaFWesKmWkod9"
+    )
+    .then(
+      () => {
+        setLoading(false);
+        alert("Thank you. I will get back to you as soon as possible.");
+
+      },
+      (error) => {
+        setLoading(false);
+        console.error(error);
+
+        alert("Ahh, something went wrong. Please try again.");
+      }
+    );
+  };
   console.log(errors);
 
   return (
@@ -17,34 +55,28 @@ export default function ContactForm() {
         type="text"
         placeholder="your name"
         {...register("name", { required: true, maxLength: 80 })}
-        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray focus:border-gray bg-transparent"
+        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
+        focus:border-gray bg-transparent"
       />
       and I want to discuss a potential project. You can email me at
-      <input
-        type="email"
-        placeholder="your@email"
-        {...register("email", {})}
-        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray focus:border-gray bg-transparent"
-      />
+      <input type="email" placeholder="your@email" {...register("email", {})}  className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
+        focus:border-gray bg-transparent"/>
       or reach out to me on
       <input
         type="tel"
         placeholder="your phone"
-        {...register("phone number", {})}
-        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray focus:border-gray bg-transparent"
+        {...register("phone", {})}
+        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
+        focus:border-gray bg-transparent"
       />
       Here are some details about my project: <br />
-      <textarea
-        {...register("project details", {})}
-        placeholder="My project is about..."
-        rows={3}
-        className="w-full outline-none border-0 p-0 mx-0 focus:ring-0 placeholder:text-lg border-b border-gray focus:border-gray bg-transparent"
-      />
-      <input
-        type="submit"
-        value="send request"
-        className="mt-8 font-medium inline-block capitalize text-lg sm:text-xl py-2 sm:py-3 px-6 sm:px-8 border-2 border-solid border-dark dark:border-light rounded cursor-pointer"
-      />
+      <textarea {...register("details", {})} 
+      placeholder="My project is about..."
+      rows={3}
+      className="w-full outline-none border-0 p-0 mx-0 focus:ring-0  placeholder:text-lg border-b border-gray 
+        focus:border-gray bg-transparent" />
+      <input type="submit" value={loading ? "Sending..." : "send request"} className="mt-8 font-medium inline-block capitalize text-lg sm:text-xl py-2 sm:py-3 px-6 sm:px-8 border-2 border-solid border-dark dark:border-light rounded cursor-pointer" />
     </form>
   );
 }
+
